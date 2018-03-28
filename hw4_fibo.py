635aef993b47ffe2de14342ebaa9f0f3dd40c8c5
Ｍing Chiao <joe@MacBook-Pro-3.local>
2018-03-28 16:20:22 +0800

    fibo fixed

--- hw4_fibo.py
+++ hw4_fibo.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 
-fibo = [0,1]
-counter = 1
 
 def compute_fibonacci(num):
     if num <= compute_fibonacci.counter:
         return compute_fibonacci.fibo[num]
     else:
-        for i in range(counter+1,num+1):
+        for i in range(compute_fibonacci.counter+1,num+1):
             result = compute_fibonacci.fibo[i-1] + compute_fibonacci.fibo[i-2]
             compute_fibonacci.fibo.append(result)
+            compute_fibonacci.counter+=1
         return result
 compute_fibonacci.counter = 1
 compute_fibonacci.fibo = [0,1]
